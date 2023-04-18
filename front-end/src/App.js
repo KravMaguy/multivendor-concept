e0018edbf776e2e4479b7c2bd2661281f2ba2c94
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2023-04-18 06:33:08 -0400

    create

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,24 @@
 import { useState } from "react";
-const todos = [
+const Todos = [
   { task: "do laundry", done: false, id: 1 },
   { task: "clean room", done: true, id: 2 },
 ];
 
 function App() {
+  const [todos, setTodos] = useState([...Todos]);
+  const [todo, setTodo] = useState("");
+  const addTodo = () => {
+    const newTodos = [
+      ...todos,
+      { task: todo, done: false, id: todos.length + 1 },
+    ];
+    setTodos(newTodos);
+  };
+  console.log(todos);
   return (
     <div>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)}></input>
+      <button onClick={() => addTodo(todo)}>+</button>
       <ul>
         {todos.map((todo) => (
           <li>{todo.task}</li>
